5a745d23dad8f6515ff684dfc45b0e12bbb3c376
realdiegor <diegoreyes150103@gmail.com>
2023-01-26 13:57:24 -0500

    add container to scroll button

--- src/components/Contact/Form/index.jsx
+++ src/components/Contact/Form/index.jsx
@@ -4,67 +4,67 @@ import { useEmailing } from "./useEmailing";
 const FormAlert = lazy(() => import("../FormAlert"));
 
 function Form() {
-  const { form, formAlert, emailInput, sendEmail } = useEmailing();
+	const { form, formAlert, emailInput, sendEmail } = useEmailing();
 
-  return (
-    <form
-      ref={form}
-      className="relative flex flex-col flex-grow p-4 shadow-md rounded-sm"
-      id="form"
-      onSubmit={sendEmail}
-    >
-      <Suspense fallback="">
-        <FormAlert formAlert={formAlert} />
-      </Suspense>
-      <label htmlFor="user_name" className="xs:text-lg lg:text-xl text-black">
-        Your Name
-      </label>
-      <input
-        type="text"
-        id="user_name"
-        name="user_name"
-        placeholder="Dana"
-        required
-        className="mt-1 px-2 py-1 rounded-sm outline-1 outline outline-midGray xl:text-lg text-black focus-within:outline-2 focus-within:outline-terciary"
-      />
-      <label
-        htmlFor="user_email"
-        className="mt-4 xs:text-lg lg:text-xl text-black"
-      >
-        Your Email
-      </label>
-      <input
-        type="email"
-        id="user_email"
-        name="user_email"
-        placeholder="example@email.com"
-        required
-        pattern="^[\w._%+-]+@[\w-]+\.[a-z]{2,}$"
-        ref={emailInput}
-        className="mt-1 px-2 py-1 rounded-sm outline-1 outline outline-midGray xl:text-lg text-black focus-within:outline-2 focus-within:outline-terciary"
-      />
-      <label
-        htmlFor="message"
-        className="mt-4 xs:text-lg lg:text-xl text-black"
-      >
-        Message
-      </label>
-      <textarea
-        name="message"
-        id="message"
-        rows="5"
-        placeholder="Hello!"
-        required
-        className="resize-y mt-1 px-2 py-1 rounded-sm outline-1 outline outline-midGray xl:text-lg text-black focus-within:outline-2 focus-within:outline-terciary"
-      ></textarea>
-      <button
-        type="submit"
-        className="mx-auto my-5 px-6 py-3 xs:text-lg bg-secondary text-white rounded-sm hover:bg-primary transition-colors duration-200 lg:text-base lg:px-6"
-      >
-        Send Email
-      </button>
-    </form>
-  );
+	return (
+		<form
+			ref={form}
+			className="relative flex flex-col flex-grow p-4 shadow-md rounded-sm"
+			id="form"
+			onSubmit={sendEmail}
+		>
+			<Suspense fallback="">
+				<FormAlert formAlert={formAlert} />
+			</Suspense>
+			<label htmlFor="user_name" className="xs:text-lg lg:text-xl text-black">
+				Your Name
+			</label>
+			<input
+				type="text"
+				id="user_name"
+				name="user_name"
+				placeholder="Dana"
+				required
+				className="mt-1 px-2 py-1 rounded-sm outline-1 outline outline-midGray xl:text-lg text-black focus-within:outline-2 focus-within:outline-terciary"
+			/>
+			<label
+				htmlFor="user_email"
+				className="mt-4 xs:text-lg lg:text-xl text-black"
+			>
+				Your Email
+			</label>
+			<input
+				type="email"
+				id="user_email"
+				name="user_email"
+				placeholder="example@email.com"
+				required
+				pattern="^[\w._%+-]+@[\w-]+\.[a-z]{2,}$"
+				ref={emailInput}
+				className="mt-1 px-2 py-1 rounded-sm outline-1 outline outline-midGray xl:text-lg text-black focus-within:outline-2 focus-within:outline-terciary"
+			/>
+			<label
+				htmlFor="message"
+				className="mt-4 xs:text-lg lg:text-xl text-black"
+			>
+				Message
+			</label>
+			<textarea
+				name="message"
+				id="message"
+				rows="5"
+				placeholder="Hello!"
+				required
+				className="resize-y mt-1 px-2 py-1 rounded-sm outline-1 outline outline-midGray xl:text-lg text-black focus-within:outline-2 focus-within:outline-terciary"
+			></textarea>
+			<button
+				type="submit"
+				className="mx-auto my-5 px-6 py-3 xs:text-lg bg-secondary text-white rounded-sm hover:bg-primary transition-colors duration-200 lg:text-base lg:px-6"
+			>
+				Send Email
+			</button>
+		</form>
+	);
 }
 
 export { Form };
--- src/components/Hero/ScrollButton/index.jsx
+++ src/components/Hero/ScrollButton/index.jsx
@@ -1,38 +1,40 @@
 import React from "react";
 
 function ScrollButton() {
-  const scrollDown = () => {
-    scrollTo({
-      left: 0,
-      top: window.innerHeight,
-      behavior: "smooth",
-    });
-  };
+	const scrollDown = () => {
+		scrollTo({
+			left: 0,
+			top: window.innerHeight,
+			behavior: "smooth",
+		});
+	};
 
-  return (
-    <button
-      className="relative row-start-4 col-start-3 justify-self-center self-center col-end-5 border-[3px] border-solid rounded-full text-white md:row-start-5 md:place-self-center hover:translate-y-1 transition-transform duration-500"
-      type="button"
-      name="Scroll Down"
-      title="Scroll Down"
-      onClick={scrollDown}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="relative top-[2px] w-12 h-12 "
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-        />
-      </svg>
-    </button>
-  );
+	return (
+		<div className="group row-start-4 col-start-3 justify-self-center self-center col-end-5 rounded-full md:row-start-5 md:place-self-center ">
+			<button
+				className="relative border-[3px] border-solid rounded-full text-white group-hover:translate-y-1 transition-transform duration-500"
+				type="button"
+				name="Scroll Down"
+				title="Scroll Down"
+				onClick={scrollDown}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.5}
+					stroke="currentColor"
+					className="relative top-[2px] w-12 h-12 "
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+					/>
+				</svg>
+			</button>
+		</div>
+	);
 }
 
 export { ScrollButton };
