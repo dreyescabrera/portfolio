7706ba44a5c8c3c5410e7c995d4cab77ab6bb8d9
realdiegor <diegoreyes150103@gmail.com>
2023-02-04 21:47:07 -0500

    add figma in skill section

--- src/components/SidebarMenu/index.jsx
+++ src/components/SidebarMenu/index.jsx
@@ -4,64 +4,64 @@ import { SidebarMediaButton } from "./SidebarMediaButton";
 import { NavItem } from "./NavItem";
 
 function SidebarMenu({ sidebarIsOpen, activeNavItem, setSidebarIsOpen }) {
-  return (
-    <aside
-      className={`fixed w-full left-0 h-screen flex flex-col justify-start items-center px-6 py-8 gap-5 overflow-auto bg-black transition-all duration-700 z-10 ${
-        sidebarIsOpen ? " left-0" : "-left-full"
-      } md:w-60  lg:w-72 2xl:w-1/6`}
-    >
-      <ProfileImage shape="circle" />
-      <h2 className="text-3xl text-lightGray font-semibold font-luxury md:text-2xl 3xl:text-4xl">
-        Diego Reyes
-      </h2>
-      <ul className="flex space-x-5">
-        <SidebarMediaButton
-          link="https://www.instagram.com/realdiegor/"
-          socialMedia="instagram"
-        />
-        <SidebarMediaButton
-          link="https://github.com/realDiegoR"
-          socialMedia="github"
-        />
-        <SidebarMediaButton
-          link="https://www.linkedin.com/in/diegoreyescabrera/"
-          socialMedia="linkedin"
-        />
-      </ul>
-      <nav className="pt-2 md:self-start lg:pt-8">
-        <ul className="flex flex-col gap-5 text-midGray font-semibold tracking-wide text-lg md:text-xl md:gap-6 3xl:text-2xl">
-          <NavItem
-            name="home"
-            id="home"
-            iconName="home"
-            activeNavItem={activeNavItem}
-            setSidebarIsOpen={setSidebarIsOpen}
-          />
-          <NavItem
-            name="who am i?"
-            id="whoami?"
-            iconName="profile"
-            activeNavItem={activeNavItem}
-            setSidebarIsOpen={setSidebarIsOpen}
-          />
-          <NavItem
-            name="projects"
-            id="projects"
-            iconName="bolt"
-            activeNavItem={activeNavItem}
-            setSidebarIsOpen={setSidebarIsOpen}
-          />
-          <NavItem
-            name="contact"
-            id="contact"
-            iconName="mail"
-            activeNavItem={activeNavItem}
-            setSidebarIsOpen={setSidebarIsOpen}
-          />
-        </ul>
-      </nav>
-    </aside>
-  );
+	return (
+		<aside
+			className={`fixed w-full left-0 h-screen flex flex-col justify-start items-center px-6 py-8 gap-5 overflow-auto bg-black transition-all duration-700 z-10 ${
+				sidebarIsOpen ? " left-0" : "-left-full"
+			} md:w-60  lg:w-72 2xl:w-1/6`}
+		>
+			<ProfileImage shape="circle" />
+			<h2 className="text-3xl text-lightGray font-semibold font-luxury md:text-2xl 3xl:text-4xl">
+				Diego Reyes
+			</h2>
+			<ul className="flex space-x-5">
+				<SidebarMediaButton
+					link="https://www.instagram.com/realdiegor/"
+					socialMedia="instagram"
+				/>
+				<SidebarMediaButton
+					link="https://github.com/realDiegoR"
+					socialMedia="github"
+				/>
+				<SidebarMediaButton
+					link="https://www.linkedin.com/in/diegoreyescabrera/"
+					socialMedia="linkedin"
+				/>
+			</ul>
+			<nav className="pt-2 md:self-start lg:pt-8">
+				<ul className="flex flex-col gap-5 text-midGray font-semibold tracking-wide text-lg md:text-xl md:gap-6 3xl:text-2xl">
+					<NavItem
+						name="home"
+						id="home"
+						iconName="home"
+						activeNavItem={activeNavItem}
+						setSidebarIsOpen={setSidebarIsOpen}
+					/>
+					<NavItem
+						name="who am i?"
+						id="whoami?"
+						iconName="profile"
+						activeNavItem={activeNavItem}
+						setSidebarIsOpen={setSidebarIsOpen}
+					/>
+					<NavItem
+						name="projects"
+						id="projects"
+						iconName="bolt"
+						activeNavItem={activeNavItem}
+						setSidebarIsOpen={setSidebarIsOpen}
+					/>
+					<NavItem
+						name="contact"
+						id="contact"
+						iconName="mail"
+						activeNavItem={activeNavItem}
+						setSidebarIsOpen={setSidebarIsOpen}
+					/>
+				</ul>
+			</nav>
+		</aside>
+	);
 }
 
 export { SidebarMenu };
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -4,26 +4,27 @@ import { TextContent } from "../common/TextContent";
 import { SkillsItem } from "./SkillsItem";
 
 function Skills() {
-  return (
-    <section className="m-sm lg:m-lg">
-      <SectionTitle text="Skills" />
-      <div className="flex flex-col gap-6 xl:flex-row">
-        <div className="xl:w-1/3">
-          <TextContent text="I have managed to gather these skills through all my never-ending process as a self-taught developer, deepening on them for being able of provide the appropriate solution to a problem." />
-        </div>
-        <ul className="xl:w-2/3 grid grid-cols-[repeat(auto-fit,_minmax(6rem,_7rem))] gap-8 justify-center">
-          <SkillsItem name="HTML" iconName="html" />
-          <SkillsItem name="CSS" iconName="css" />
-          <SkillsItem name="SASS" iconName="sass" />
-          <SkillsItem name="JavaScript" iconName="javascript" />
-          <SkillsItem name="ReactJS" iconName="react" />
-          <SkillsItem name="Tailwind CSS" iconName="tailwind" />
-          <SkillsItem name="Vite" iconName="vite" />
-          <SkillsItem name="Git" iconName="git" />
-        </ul>
-      </div>
-    </section>
-  );
+	return (
+		<section className="m-sm lg:m-lg">
+			<SectionTitle text="Skills" />
+			<div className="flex flex-col gap-6 xl:flex-row">
+				<div className="xl:w-1/3">
+					<TextContent text="I have managed to gather these skills through all my never-ending process as a self-taught developer, deepening on them for being able of provide the appropriate solution to a problem." />
+				</div>
+				<ul className="xl:w-2/3 grid grid-cols-[repeat(auto-fit,_minmax(6rem,_7rem))] gap-8 justify-center">
+					<SkillsItem name="HTML" iconName="html" />
+					<SkillsItem name="CSS" iconName="css" />
+					<SkillsItem name="SASS" iconName="sass" />
+					<SkillsItem name="JavaScript" iconName="javascript" />
+					<SkillsItem name="ReactJS" iconName="react" />
+					<SkillsItem name="Tailwind" iconName="tailwind" />
+					<SkillsItem name="Vite" iconName="vite" />
+					<SkillsItem name="Figma" iconName="figma" />
+					<SkillsItem name="Git" iconName="git" />
+				</ul>
+			</div>
+		</section>
+	);
 }
 
 export { Skills };
