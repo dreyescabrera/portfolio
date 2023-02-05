fb6626f1781ef86df194a1e852c5bfcc6d88ac66
realdiegor <diegoreyes150103@gmail.com>
2023-02-05 17:28:17 -0500

    add sessionStorage

--- src/components/App/useIntersectionObserver.js
+++ src/components/App/useIntersectionObserver.js
@@ -1,42 +1,47 @@
 import React from "react";
 
 function useIntersectionObserver() {
-  const homeRef = React.useRef(null);
-  const whoamiRef = React.useRef(null);
-  const projectsRef = React.useRef(null);
-  const contactRef = React.useRef(null);
+	const homeRef = React.useRef(null);
+	const whoamiRef = React.useRef(null);
+	const projectsRef = React.useRef(null);
+	const contactRef = React.useRef(null);
 
-  const [activeNavItem, setActiveNavItem] = React.useState("home");
-  const lightUpNavItem = (item) => {
-    setActiveNavItem(item);
-  };
+	const [activeNavItem, setActiveNavItem] = React.useState(
+		sessionStorage.activeNavItem ? sessionStorage.activeNavItem : "home"
+	);
 
-  React.useEffect(() => {
-    const options = {
-      threshold: [0.25, 0.8], //0.25 for normal scroll, 0.8 for bugfixing on scrolling down to about section and not ever leaving home section entirely
-    };
+	const lightUpNavItem = (item) => {
+		setActiveNavItem(item);
+		sessionStorage.setItem("activeNavItem", item);
+		console.log(sessionStorage);
+	};
 
-    const callback = (entries) => {
-      const currentSection = entries[0];
-      const intersectionRatio = currentSection.intersectionRatio;
-      if (intersectionRatio > 0.25 && intersectionRatio < 0.6) {
-        lightUpNavItem(currentSection.target.id);
-        return;
-      }
-      if (intersectionRatio > 0.8) {
-        //only for scrolling up to home section
-        lightUpNavItem(currentSection.target.id);
-      }
-    };
+	React.useEffect(() => {
+		const options = {
+			threshold: [0.25, 0.8], //0.25 for normal scroll, 0.8 for bugfixing on scrolling down to about section and not ever leaving home section entirely
+		};
 
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(homeRef.current);
-    observer.observe(whoamiRef.current);
-    observer.observe(projectsRef.current);
-    observer.observe(contactRef.current);
-  }, []);
+		const callback = (entries) => {
+			const currentSection = entries[0];
+			const intersectionRatio = currentSection.intersectionRatio;
+			if (intersectionRatio > 0.25 && intersectionRatio < 0.6) {
+				lightUpNavItem(currentSection.target.id);
+				return;
+			}
+			if (intersectionRatio > 0.8) {
+				//only for scrolling up to home section
+				lightUpNavItem(currentSection.target.id);
+			}
+		};
 
-  return { activeNavItem, homeRef, whoamiRef, projectsRef, contactRef };
+		const observer = new IntersectionObserver(callback, options);
+		observer.observe(homeRef.current);
+		observer.observe(whoamiRef.current);
+		observer.observe(projectsRef.current);
+		observer.observe(contactRef.current);
+	}, []);
+
+	return { activeNavItem, homeRef, whoamiRef, projectsRef, contactRef };
 }
 
 export { useIntersectionObserver };
