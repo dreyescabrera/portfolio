84d65600771b9ccd51dbd8dfe67b98d5ea94e796
realdiegor <diegoreyes150103@gmail.com>
2023-02-11 11:55:19 -0500

    Fix hover on project button

--- src/components/Projects/ProjectButton/index.jsx
+++ src/components/Projects/ProjectButton/index.jsx
@@ -9,7 +9,7 @@ function ProjectButton({ link, text, mainCtA }) {
 			className={`${
 				mainCtA
 					? "bg-secondary text-white border-2 border-transparent hover:bg-primary dark:text-primary dark:bg-darkPrimary dark:hover:bg-lightGray"
-					: "text-terciary border-terciary border-2 dark:hover:bg-[rgba(51,101,176,0.3)]  dark:text-lightGray dark:border-darkSecondary"
+					: "text-terciary border-terciary border-2 hover:bg-quintenary dark:hover:bg-[rgba(51,101,176,0.3)]  dark:text-lightGray dark:border-darkSecondary"
 			} px-4 py-2 text-sm xs:text-base rounded-sm transition-colors duration-200 lg:text-base lg:px-3`}
 		>
 			{text}
