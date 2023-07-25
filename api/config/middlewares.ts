4307f76df714199a841fc55f9f7a817b9c450227
realdiegor <diegoreyes150103@gmail.com>
2023-07-24 22:29:39 -0400

    feat: create strapi app
    created article content-type and added first two articles

---
+++ api/config/middlewares.ts
@@ -0,0 +1,31 @@
+export default [
+	'strapi::errors',
+	{
+		name: 'strapi::security',
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					'connect-src': ["'self'", 'https:'],
+					'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'market-assets.strapi.io'],
+					'media-src': [
+						"'self'",
+						'data:',
+						'blob:',
+						'res.cloudinary.com',
+						'market-assets.strapi.io',
+					],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+	'strapi::cors',
+	'strapi::poweredBy',
+	'strapi::logger',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
+];
