959622f7898bc1d1cddc4af003a088431171649e
realdiegor <diegoreyes150103@gmail.com>
2023-08-11 16:11:32 -0400

    chore(strapi): migrate from sqlite to postgres database

---
+++ apps/strapi/config/env/production/database.ts
@@ -0,0 +1,19 @@
+import { parse } from 'pg-connection-string';
+const config = parse(process.env.DATABASE_URL);
+
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
+  },
+});
--- apps/strapi/config/env/production/server.ts
+++ apps/strapi/config/env/production/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
