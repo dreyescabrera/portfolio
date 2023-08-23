62e3303a368f1f91d46151ab8cd4910ef73265ef
realdiegor <diegoreyes150103@gmail.com>
2023-08-23 16:38:32 -0400

    fix(web): include .ts files in codegen config file

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
 	overwrite: true,
 	schema: 'http://localhost:1337/graphql',
-	documents: ['src/**/*.tsx'],
+	documents: ['src/**/*.tsx', 'src/**/*.ts'],
 	generates: {
 		'src/services/graphql/': {
 			preset: 'client',
